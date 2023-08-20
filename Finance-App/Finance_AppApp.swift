21331725b9e134f8a58a2f0dac03f6ec10966d5a
LuiArm <Luisarmendariz24@icloud.com>
2023-08-20 15:54:23 -0600

    Initial Commit

---
+++ Finance-App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance-App
+//
+//  Created by luis armendariz on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
